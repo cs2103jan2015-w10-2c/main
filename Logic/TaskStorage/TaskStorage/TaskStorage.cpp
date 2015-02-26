36e700c8ec8a19bb2ab1a8c82a74b505b27cded6
JingyiLiang <jingyi_zoe@hotmail.com>
2015-02-27 02:57:04 +0800

    comment added

--- Logic/TaskStorage/TaskStorage/TaskStorage.cpp
+++ Logic/TaskStorage/TaskStorage/TaskStorage.cpp
@@ -4,10 +4,15 @@
 TaskStorage::TaskStorage(void){}
 
 void TaskStorage::parseInput(string input){
+	//get the first word before the first white space
 	string command;
 	size_t pos1 = input.find_first_of(' ');
 	_commandType = input.substr(0, pos1);
 
+	//check the number of devider "//"
+	//no devider -> floating: description
+	//one devider -> deadline: endTime//description
+	//two devider -> timed: startTime//endTime//description
 	string devider = "//";
 	size_t pos2 = input.find_first_of(devider);
 	if(pos2 == string::npos){
