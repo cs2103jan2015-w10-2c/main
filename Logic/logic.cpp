4c5ec77cfff548916575ac86b247d6cbd10a7f30
unknown <nathanthandoko@gmail.com>
2015-03-08 23:59:17 +0800

    call float, timed, deadline add. Give output message

--- Logic/logic.cpp
+++ Logic/logic.cpp
@@ -1,29 +1,45 @@
 #include "Task.h"
 #include "CommandParser.h"
+#include "Storage.h"
 
 
 //call ParseCommand and receive task from ParseCommand
 Task t;
-CommandParser cp;
-t = CommandParser::cp.createTask(input);
+Storage s;
 
 //identify commandType and required parameters and call storeFloat(string taskName)
-if (t.getTaskType() == "FloatingTask"){
-	storeFloat(getTaskName());
-}	else if(t.getTaskType() == "TimedTask"){
-	storeTimed(getTaskName(), getDate(), getStartTime(), getEndTime());
-}	else if (t.getTaskType() == "DeadlineTask"){
-	storeDeadline(getTaskName(), getDate(), getEndTime());
+void callTask(){
+	if (t.getTaskType() == "FloatingTask"){
+		s.storeFloat(t.getName());
+	}	else if(t.getTaskType() == "TimedTask"){
+		s.storeTimed(t.getName(), t.getDate(), t.getStartTime(), t.getEndTime());
+	}	else if (t.getTaskType() == "DeadlineTask"){
+		s.storeDeadline(t.getName(), t.getDate(), t.getEndTime());
+	}
+}
+
+bool isDuplicate(){
+	if (s.isFloatDuplicate(t.getName()) || s.isTimedDuplicate() || s.isDeadlineDuplicate()){
+		return true;
+	}
+		else return false;
+
 }
 
 //receive bool from storeFloat and create output message
-if (isStored()){
+void tellUI(){
 	ostringstream os;
-	os << t.getTaskName() << "has been stored successfully";
+	if (!isDuplicate(t.getName())){
+		os << t.getName() << "has been stored successfully";
+	}
+		else {
+			os << "Sorry, the task has not been stored successfully";
+		} 
+
+		showToUser(os.str());
+		// this should call a function in the UI to show output message
 }
-	else {
-		os << "Sorry, the task has not been stored successfully";
-	} 
 
-	showToUser(os.str());
-
+void showToUser(string outputMessage){
+	cout << outputMessage;
+}
