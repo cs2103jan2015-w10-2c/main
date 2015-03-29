1855d428b56ba1c610b25058fb453c4f635af745
unknown <nathanthandoko@gmail.com>
2015-03-30 02:00:40 +0800

    changes to commandParser class, now can write today, tomorrow, Monday, Tuesday etc

--- All source files and header files/CommandParser.cpp
+++ All source files and header files/CommandParser.cpp
@@ -1,4 +1,6 @@
 #include "CommandParser.h"
+#include <atltime.h>
+#include <math.h>
 #include <ctime>
 
 using namespace std;
@@ -7,13 +9,14 @@ using namespace std;
 string CommandParser::taskType;
 string CommandParser::commandType;
 string CommandParser::name;
-int CommandParser::year;
+int CommandParser::year = 0;
 int CommandParser::month;
 int CommandParser::day;
 int CommandParser::startTimeHour;
 int CommandParser::startTimeMin;
 int CommandParser::endTimeHour;
 int CommandParser::endTimeMin;
+string CommandParser::dayOfWeek;
 
 
 void CommandParser::identifyTask(string userInput) {
@@ -25,15 +28,16 @@ void CommandParser::identifyTask(string userInput) {
 	//convert all letters in the commandType to lower case
 	transform(commandType.begin(), commandType.end(), commandType.begin(), ::tolower);
 	assert(commandType != "");
-	
+
 	if(commandType == "add"){
 	//check the number of devider "//"
 	//no devider -> floating: description
 	//one devider -> deadline: endTime//description
 	//two devider -> timed: startTime//endTime//description
-
+	
 	string devider = "/";
 	size_t posD1 = userInput.find(devider);
+
 	if(posD1 == string::npos) {
 		taskType = "FloatingTask";
 		name = userInput.substr(pos1+1);
@@ -49,7 +53,8 @@ void CommandParser::identifyTask(string userInput) {
 		size_t posD5 = userInput.find(devider, posD4+1);
 		size_t posD6 = userInput.find(devider, posD5+1);
 
-		if (userInput.substr(pos1+1, (posD1-pos1)) == "today"){
+		if (userInput.substr(pos1+1, (posD1-pos1-1)) == "today"){
+
 			time_t now = time(0);	
 			struct tm time;
 			localtime_s(&time, &now);
@@ -57,37 +62,97 @@ void CommandParser::identifyTask(string userInput) {
 			month = time.tm_mon + 1;
 			year = time.tm_year + 1900;
 		}	
-		else if (userInput.substr(pos1+1, (posD1-pos1)) == "tomorrow"){
+		else if (userInput.substr(pos1+1, (posD1-pos1-1)) == "tomorrow"){
 			time_t now = time(0);	
 			struct tm time;
 			localtime_s(&time, &now);
 			day =  time.tm_mday + 1;
-			month = time.tm_mon + 1 + 1;
-			year = time.tm_year + 1900 + 1;
+			month = time.tm_mon + 1;
+			year = time.tm_year + 1900;
 		}
 			else {
-			year = stoi(userInput.substr(pos1+1, (posD1-pos1)));
-			month =  stoi(userInput.substr(posD1+1, (posD2-posD1)));
-			day =  stoi(userInput.substr(posD2+1, (posD3-posD2)));
+				dayOfWeek = userInput.substr(pos1+1, (posD1-pos1-1));
+				int dayOfWeekNo = 0;
+				if (dayOfWeek == "Monday"){
+					dayOfWeekNo = 1;
+				}
+				else if (dayOfWeek == "Tuesday"){
+					dayOfWeekNo = 2;
+				}
+				else if (dayOfWeek == "Wednesday"){
+					dayOfWeekNo = 3;
+				}
+				else if (dayOfWeek == "Thursday"){
+					dayOfWeekNo = 4;
+				}
+				else if (dayOfWeek == "Friday"){
+					dayOfWeekNo = 5;
+				}
+				else if (dayOfWeek == "Saturday"){
+					dayOfWeekNo = 6;
+				}
 
-				if(posD6 == string::npos) {
-					taskType = "DeadlineTask";
-					endTimeHour = stoi(userInput.substr(posD3+1, (posD4-posD3)));
-					endTimeMin = stoi(userInput.substr(posD4+1, (posD5-posD4)));
-					name = userInput.substr(posD5+1);
+				time_t now = time(0);	
+				int dayToAdd = 0;
+				struct tm time;
+				localtime_s(&time, &now);
+				
+				if (time.tm_wday > dayOfWeekNo){
+					dayToAdd = 7 - abs(dayOfWeekNo - time.tm_wday);
+				}
+					else {
+						dayToAdd = dayOfWeekNo - time.tm_wday;
+					}
 
-				} else {
-					size_t posD7 = userInput.find(devider, posD6+1);
+				time.tm_mday =  time.tm_mday + dayToAdd;
 
-					taskType = "TimedTask";
-					startTimeHour = stoi(userInput.substr(posD3+1, (posD4-posD3)));
-					startTimeMin = stoi(userInput.substr(posD4+1, (posD5-posD4)));
-					endTimeHour = stoi(userInput.substr(posD5+1, (posD6-posD5)));
-					endTimeMin = stoi(userInput.substr(posD6+1, (posD7-posD6)));
-					name = userInput.substr(posD7+1);
-				}	
+				mktime(&time);
+				day = time.tm_mday;
+				month = time.tm_mon + 1;
+				year = time.tm_year + 1900;
 			}
+		if(posD4 == string::npos) {
+			taskType = "DeadlineTask";
+			endTimeHour = stoi(userInput.substr(posD1+1, (posD2-posD1)));
+			endTimeMin = stoi(userInput.substr(posD2+1, (posD3-posD2)));
+			name = userInput.substr(posD3+1);
 		}
+			else {
+				taskType = "TimedTask";
+				startTimeHour = stoi(userInput.substr(posD1+1, (posD2-posD1)));
+				startTimeMin = stoi(userInput.substr(posD2+1, (posD3-posD2)));
+				endTimeHour = stoi(userInput.substr(posD3+1, (posD4-posD3)));
+				endTimeMin = stoi(userInput.substr(posD4+1, (posD5-posD4)));
+				name = userInput.substr(posD5+1);
+			}	
+
+			if (stoi(userInput.substr(pos1+1, (posD1-pos1-1))) < 22000){
+				year = stoi(userInput.substr(pos1+1, (posD1-pos1)));
+				month = stoi(userInput.substr(posD1+1, (posD2-posD1)));
+				day =  stoi(userInput.substr(posD2+1, (posD3-posD2)));
+
+					if(posD6 == string::npos) {
+						taskType = "DeadlineTask";
+						startTimeHour = 0;
+						startTimeMin = 0;
+						endTimeHour = stoi(userInput.substr(posD3+1, (posD4-posD3)));
+						endTimeMin = stoi(userInput.substr(posD4+1, (posD5-posD4)));
+						name = userInput.substr(posD5+1);
+
+					} else {
+						size_t posD7 = userInput.find(devider, posD6+1);
+
+						taskType = "TimedTask";
+						startTimeHour = stoi(userInput.substr(posD3+1, (posD4-posD3)));
+						startTimeMin = stoi(userInput.substr(posD4+1, (posD5-posD4)));
+						endTimeHour = stoi(userInput.substr(posD5+1, (posD6-posD5)));
+						endTimeMin = stoi(userInput.substr(posD6+1, (posD7-posD6)));
+						name = userInput.substr(posD7+1);
+					}	
+			}
+			
+	}
+		
 	}else if(commandType == "delete" || commandType == "search" || commandType == "done" || commandType == "notdone" ){
 		name = userInput.substr(pos1+1);
 	}else if(commandType == "sort" || commandType == "display"){
