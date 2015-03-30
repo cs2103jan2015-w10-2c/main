1c7be154747d6b964fc5da0a3dcdc3fbeca61b83
Dong Peisen/n <dps1026@gmail.com/n>
2015-03-30 14:02:02 +0800

    command parser updates

--- All source files and header files/CommandParser.cpp
+++ All source files and header files/CommandParser.cpp
@@ -38,43 +38,42 @@ void CommandParser::identifyTask(string userInput) {
 	//one devider -> deadline: endTime//description
 	//two devider -> timed: startTime//endTime//description
 	
-	string devider = "/";
-	size_t posD1 = userInput.find(devider);
+		string devider = "/";
+		size_t posD1 = userInput.find(devider);
 
-	if(posD1 == string::npos) {
-		taskType = "FloatingTask";
-		name = userInput.substr(pos1+1);
-		return;
+		if(posD1 == string::npos) {
+			taskType = "FloatingTask";
+			name = userInput.substr(pos1+1);
+			return;
 
-	} else {
-		//add 2015//01//02//8//0//meeting
-		//add 2015//01//02//8//0//11//0//meeting
-		//NOTE: doesn't cover the case where the task type is invalid.
+		} else {
+			//add 2015//01//02//8//0//meeting
+			//add 2015//01//02//8//0//11//0//meeting
+			//NOTE: doesn't cover the case where the task type is invalid.
 
-		size_t posD2 = userInput.find(devider, posD1+1);
-		size_t posD3 = userInput.find(devider, posD2+1);
-		size_t posD4 = userInput.find(devider, posD3+1);
-		size_t posD5 = userInput.find(devider, posD4+1);
-		size_t posD6 = userInput.find(devider, posD5+1);
+			size_t posD2 = userInput.find(devider, posD1+1);
+			size_t posD3 = userInput.find(devider, posD2+1);
+			size_t posD4 = userInput.find(devider, posD3+1);
+			size_t posD5 = userInput.find(devider, posD4+1);
+			size_t posD6 = userInput.find(devider, posD5+1);
 
-		if (userInput.substr(pos1+1, (posD1-pos1-1)) == "today"){
+			if (userInput.substr(pos1+1, (posD1-pos1-1)) == "today"){
 
-			time_t now = time(0);	
-			struct tm time;
-			localtime_s(&time, &now);
-			day =  time.tm_mday;
-			month = time.tm_mon + 1;
-			year = time.tm_year + 1900;
-		}	
-		else if (userInput.substr(pos1+1, (posD1-pos1-1)) == "tomorrow"){
-			time_t now = time(0);	
-			struct tm time;
-			localtime_s(&time, &now);
-			day =  time.tm_mday + 1;
-			month = time.tm_mon + 1;
-			year = time.tm_year + 1900;
-		}
-			else {
+				time_t now = time(0);	
+				struct tm time;
+				localtime_s(&time, &now);
+				day =  time.tm_mday;
+				month = time.tm_mon + 1;
+				year = time.tm_year + 1900;
+			}	
+			else if (userInput.substr(pos1+1, (posD1-pos1-1)) == "tomorrow"){
+				time_t now = time(0);	
+				struct tm time;
+				localtime_s(&time, &now);
+				day =  time.tm_mday + 1;
+				month = time.tm_mon + 1;
+				year = time.tm_year + 1900;
+			} else {
 				dayOfWeek = userInput.substr(pos1+1, (posD1-pos1-1));
 				int dayOfWeekNo = 0;
 				if (dayOfWeek == "Monday"){
@@ -100,29 +99,27 @@ void CommandParser::identifyTask(string userInput) {
 				int dayToAdd = 0;
 				struct tm time;
 				localtime_s(&time, &now);
-				
+			
 				if (time.tm_wday > dayOfWeekNo){
 					dayToAdd = 7 - abs(dayOfWeekNo - time.tm_wday);
-				}
-					else {
+				} else {
 						dayToAdd = dayOfWeekNo - time.tm_wday;
-					}
+				}
 
 				time.tm_mday =  time.tm_mday + dayToAdd;
-
 				mktime(&time);
 				day = time.tm_mday;
 				month = time.tm_mon + 1;
 				year = time.tm_year + 1900;
 			}
-		if(posD4 == string::npos) {
-			taskType = "DeadlineTask";
-			endTimeHour = stoi(userInput.substr(posD1+1, (posD2-posD1)));
-			endTimeMin = stoi(userInput.substr(posD2+1, (posD3-posD2)));
-			name = userInput.substr(posD3+1);
-			return;
-		}
-			else if ((userInput.substr(pos1+1, (posD1-pos1-1)))[0] != '2') {
+
+			if(posD4 == string::npos) {
+				taskType = "DeadlineTask";
+				endTimeHour = stoi(userInput.substr(posD1+1, (posD2-posD1)));
+				endTimeMin = stoi(userInput.substr(posD2+1, (posD3-posD2)));
+				name = userInput.substr(posD3+1);
+				return;
+			} else if ((userInput.substr(pos1+1, (posD1-pos1-1)))[0] != '2') {
 				taskType = "TimedTask";
 				startTimeHour = stoi(userInput.substr(posD1+1, (posD2-posD1)));
 				startTimeMin = stoi(userInput.substr(posD2+1, (posD3-posD2)));
@@ -132,32 +129,29 @@ void CommandParser::identifyTask(string userInput) {
 				return;
 			}	
 			
-			
-				year = stoi(userInput.substr(pos1+1, (posD1-pos1)));
-				month = stoi(userInput.substr(posD1+1, (posD2-posD1)));
-				day =  stoi(userInput.substr(posD2+1, (posD3-posD2)));
+			year = stoi(userInput.substr(pos1+1, (posD1-pos1)));
+			month = stoi(userInput.substr(posD1+1, (posD2-posD1)));
+			day =  stoi(userInput.substr(posD2+1, (posD3-posD2)));
 
-					if(posD6 == string::npos) {
-						taskType = "DeadlineTask";
-						startTimeHour = 0;
-						startTimeMin = 0;
-						endTimeHour = stoi(userInput.substr(posD3+1, (posD4-posD3)));
-						endTimeMin = stoi(userInput.substr(posD4+1, (posD5-posD4)));
-						name = userInput.substr(posD5+1);
+			if(posD6 == string::npos) {
+				taskType = "DeadlineTask";
+				startTimeHour = 0;
+				startTimeMin = 0;
+				endTimeHour = stoi(userInput.substr(posD3+1, (posD4-posD3)));
+				endTimeMin = stoi(userInput.substr(posD4+1, (posD5-posD4)));
+				name = userInput.substr(posD5+1);
 
-					} else {
-						size_t posD7 = userInput.find(devider, posD6+1);
+			} else {
+				size_t posD7 = userInput.find(devider, posD6+1);
 
-						taskType = "TimedTask";
-						startTimeHour = stoi(userInput.substr(posD3+1, (posD4-posD3)));
-						startTimeMin = stoi(userInput.substr(posD4+1, (posD5-posD4)));
-						endTimeHour = stoi(userInput.substr(posD5+1, (posD6-posD5)));
-						endTimeMin = stoi(userInput.substr(posD6+1, (posD7-posD6)));
-						name = userInput.substr(posD7+1);
-					}	
-			
-			
-	}	
+				taskType = "TimedTask";
+				startTimeHour = stoi(userInput.substr(posD3+1, (posD4-posD3)));
+				startTimeMin = stoi(userInput.substr(posD4+1, (posD5-posD4)));
+				endTimeHour = stoi(userInput.substr(posD5+1, (posD6-posD5)));
+				endTimeMin = stoi(userInput.substr(posD6+1, (posD7-posD6)));
+				name = userInput.substr(posD7+1);
+			}	
+		}	
 	} else if (commandType == "delete" || commandType == "done" || commandType == "notdone") {
 		if (isalpha(userInput.at(pos1+1))) {
 			name = userInput.substr(pos1+1);
@@ -175,12 +169,6 @@ void CommandParser::identifyTask(string userInput) {
 	return;
 }
 
-CommandParser::CommandParser(void) { }
-
-
-CommandParser::~CommandParser(void) { }
-
-
 string CommandParser::getCommandType() {
 	return commandType;
 }
@@ -188,8 +176,6 @@ string CommandParser::getCommandType() {
 string CommandParser::getTaskType() {
 	return taskType;
 }
-
-
 
 string CommandParser::getDate() {
 	stringstream s;
@@ -220,3 +206,7 @@ int CommandParser::getStartTimeMin() {
 int CommandParser::getYear(){
 	return year;
 }
+
+CommandParser::CommandParser(void) { }
+
+CommandParser::~CommandParser(void) { }
