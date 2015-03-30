80b4bf8a90eb96e91e5bd98e6633abf6d5518cde
unknown <nathanthandoko@gmail.com>
2015-03-30 13:06:16 +0800

    merge fix to commandParser

--- All source files and header files/CommandParser.cpp
+++ All source files and header files/CommandParser.cpp
@@ -1,16 +1,14 @@
 #include "CommandParser.h"
-<<<<<<< HEAD
+
 #include <atltime.h>
 #include <math.h>
 #include <locale>
 #include <ctime>
-=======
->>>>>>> a6fc0fec54498484374b791c20c32cf7d8cd9a63
 
 using namespace std;
 
 //string CommandParser::userInput;
-<<<<<<< HEAD
+
 string CommandParser::taskType="";
 string CommandParser::commandType="";
 string CommandParser::name="";
@@ -24,20 +22,6 @@ int CommandParser::endTimeMin=-1;
 string CommandParser::dayOfWeek;
 int CommandParser::number=-1;
 
-=======
-string CommandParser::taskType = "";
-string CommandParser::commandType = "";
-string CommandParser::name = "";
-int CommandParser::number = -1; //for delete
-int CommandParser::year = -1;
-int CommandParser::month = -1;
-int CommandParser::day = -1;
-int CommandParser::startTimeHour = -1;
-int CommandParser::startTimeMin = -1;
-int CommandParser::endTimeHour = -1;
-int CommandParser::endTimeMin = -1;
->>>>>>> a6fc0fec54498484374b791c20c32cf7d8cd9a63
-
 void CommandParser::identifyTask(string userInput) {
 
 	//get the first word before the first white space
@@ -47,13 +31,8 @@ void CommandParser::identifyTask(string userInput) {
 	//convert all letters in the commandType to lower case
 	transform(commandType.begin(), commandType.end(), commandType.begin(), ::tolower);
 	assert(commandType != "");
-<<<<<<< HEAD
 
 	if(commandType == "add"){
-=======
-	
-	if (commandType == "add") {
->>>>>>> a6fc0fec54498484374b791c20c32cf7d8cd9a63
 	//check the number of devider "//"
 	//no devider -> floating: description
 	//one devider -> deadline: endTime//description
@@ -61,12 +40,8 @@ void CommandParser::identifyTask(string userInput) {
 	
 	string devider = "/";
 	size_t posD1 = userInput.find(devider);
-<<<<<<< HEAD
 
 	if(posD1 == string::npos) {
-=======
-	if (posD1 == string::npos) {
->>>>>>> a6fc0fec54498484374b791c20c32cf7d8cd9a63
 		taskType = "FloatingTask";
 		name = userInput.substr(pos1+1);
 		return;
@@ -82,7 +57,6 @@ void CommandParser::identifyTask(string userInput) {
 		size_t posD5 = userInput.find(devider, posD4+1);
 		size_t posD6 = userInput.find(devider, posD5+1);
 
-<<<<<<< HEAD
 		if (userInput.substr(pos1+1, (posD1-pos1-1)) == "today"){
 
 			time_t now = time(0);	
@@ -183,47 +157,15 @@ void CommandParser::identifyTask(string userInput) {
 					}	
 			
 			
-	}
-		
-	}else if(commandType == "delete" || commandType == "search" || commandType == "done" || commandType == "notdone" ){
-		name = userInput.substr(pos1+1);
-	}else if(commandType == "sort" || commandType == "display"){
-		if (isalpha(userInput.at(pos1+1))){
-=======
-		year = stoi(userInput.substr(pos1+1, (posD1-pos1)));
-		month =  stoi(userInput.substr(posD1+1, (posD2-posD1)));
-		day =  stoi(userInput.substr(posD2+1, (posD3-posD2)));
-
-		if (posD6 == string::npos) {
-			taskType = "DeadlineTask";
-			endTimeHour = stoi(userInput.substr(posD3+1, (posD4-posD3)));
-			endTimeMin = stoi(userInput.substr(posD4+1, (posD5-posD4)));
-			name = userInput.substr(posD5+1);
-
-		} else {
-			size_t posD7 = userInput.find(devider, posD6+1);
-
-			taskType = "TimedTask";
-			startTimeHour = stoi(userInput.substr(posD3+1, (posD4-posD3)));
-			startTimeMin = stoi(userInput.substr(posD4+1, (posD5-posD4)));
-			endTimeHour = stoi(userInput.substr(posD5+1, (posD6-posD5)));
-			endTimeMin = stoi(userInput.substr(posD6+1, (posD7-posD6)));
-			name = userInput.substr(posD7+1);
-		}	
-	}
-	} else if (commandType == "search") {
-		name = userInput.substr(pos1+1);
+	}	
 	} else if (commandType == "delete" || commandType == "done" || commandType == "notdone") {
 		if (isalpha(userInput.at(pos1+1))) {
->>>>>>> a6fc0fec54498484374b791c20c32cf7d8cd9a63
 			name = userInput.substr(pos1+1);
 		} else {
 			number = stoi(userInput.substr(pos1+1));
 		}
-<<<<<<< HEAD
-=======
+
 	} else if (commandType == "sort" || commandType == "display") {
->>>>>>> a6fc0fec54498484374b791c20c32cf7d8cd9a63
 		return;
 	} else if (commandType == "exit") {
 		return;
