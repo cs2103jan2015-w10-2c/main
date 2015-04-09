cd4c23420077d085b7989dab87a8b5f2ac738aee
Teck Kian <teckkian249@gmail.com>
2015-04-09 16:30:26 +0800

    update storage for unit testing

--- Storage/Storage/Storage.h
+++ Storage/Storage/Storage.h
@@ -27,23 +27,52 @@ private:
 
 	static const string LINE_BUFFER;
 	static const string COMMANDLIST;
+	static const string PATH_NAME_BUFFER;
+
+	static const int MIN_MONTHS_IN_A_YEAR;
+	static const int MAX_MONTHS_IN_A_YEAR;
+	static const int MIN_DAY_IN_A_MONTH;
+	static const int MAX_DAYS_IN_A_MONTH[12];
+	static const int MIN_HOURS_IN_A_DAY;
+	static const int MAX_HOURS_IN_A_DAY;
+	static const int MIN_MINUTES_IN_AN_HOUR;
+	static const int MAX_MINUTES_IN_AN_HOUR;
+
+	static const int SHIFT_BY_ONE;
+
+	static const string FLOATING_TASK;
+	static const string DEADLINE_TASK;
+	static const string TIMED_TASK;
 
 public:
 
 	Storage();
 	static string _fileName;
+	static string _pathName;
 	static list<Task>_taskList;
 	static list<Task>_searchResultList;
 	static ofstream _fWrite;
 	static ifstream _fRead;
 	static char buffer[1000];
+	static int _index;
 
 	static bool isSuccess;
 	static bool isSearched;
+	static bool isDateValid;
+	static bool isTimeValid;
 	static bool compareTask(Task task);
 	static void getTask(Task task);
 	static void storePreviousTask(string commandType);
 	static list<Task> getPreviousTaskList();
+	static list<Task> autoInitialDisplay();
+	static list<Task> allDeadline();
+	static list<Task> allFloating();
+	static list<Task> allTimed();
+	static list<Task> allToday();
+	static list<Task> allTomorrow();
+	static list<Task> allYesterday();
+	static void searchTodayTask(int day, int month, int year);
+	static void searchUpcomingDeadline(int day, int month, int year);
 	static void getPosition(int i);
 
 	static void findTaskInList(string searchKeyWord);
@@ -51,32 +80,37 @@ public:
 	static bool isFloatDuplicate(Task task);
 	static bool isTimedDuplicate(Task task);
 	static bool isDeadlineDuplicate(Task task);
-	static void showDirectory();
-	static bool isExistingFile();
+	static bool isValidDate(Task task);
+	static bool isValidTime(Task task);
+	static bool showDirectory();
 	static void openFile();
 	static void readFile();
 	static void setFileName(string name);
+	static void setPathName(string name);
 	static void writeToFile();
 	static string toLower(string text);
 	static string getCommandList();
 
 
 	static void creatRecordAdd(Task task);
-	static void addToTracker(Record record1);
+	static void addToTracker();
 
 	static void sortList();
 	static void undoingReverseAdd(list<Task> listToUndo);
 	static void undoingReverseDelete(list<Task> listToUndo);
 	static void undoingReverseNotDone(list<Task> listToUndo);
 	static void undoingReverseDone(list<Task> listToUndo);
+	static void undoingReverseEdit(list<Task> listToUndo);
 
 	static void storeTask(Task task);
-	static string editTask(int i, string s);
+	static string editTaskName(int i, string s);
+	static string editTaskTime(int i, double sth, double stm, double eth, double etm);
+	static string editTaskDate(int i, int year, int month, int day);
 	static string searchByName(string searchKeyWord);
 	static string markDone(int i);
 	static string markNotDone(int i);
 	static string deleteByNumber(int i);
-	static int getIndex();
+	static void getIndex(Task task);
 
 
 	static string toStringTaskDetail();
