45589653405cb889bc773c4e282863d79dba6b04
JingyiLiang <jingyi_zoe@hotmail.com>
2015-04-11 21:06:06 +0800

    jingyi unit tests for Tracker and Record

--- v0.1 test/V0.1 test run/UnitTest_Jingyi/unittest_Jingyi.cpp
+++ v0.1 test/V0.1 test run/UnitTest_Jingyi/unittest_Jingyi.cpp
@@ -10,62 +10,106 @@ namespace UnitTest_Jingyi
 	TEST_CLASS(RecordTest)
 	{
 	public:
-		
+
 		TEST_METHOD(RecordConstructorTest){
 			// TODO: Your test code here
-			Task task1 = Task("add", "CS meeting");
-			Task task2 = Task("add", "CS lecture");
+			string name1, name2, command1, command2;
+			name1 = "CS meeting";
+			name2 = "Cs lecture";
+			command1 = "add";
+			command2 = "add";
+			Task task1 = Task(command1, name1);
+			Task task2 = Task(command2, name2);
+
 			list<Task> taskList1, taskList2;
 			taskList1.push_back(task1);
 
 			taskList2.push_back(task1);
 			taskList2.push_back(task2);
 
-			Record record1 = Record("add", task1);
-			Record record2 = Record("delete", taskList2);
+			string commandType1, commandType2;
+			commandType1 = "add";
+			commandType2 = "delete";
+
+			Record record1 = Record(commandType1, task1);
+			Record record2 = Record(commandType2, taskList2);
 
 			list<Task> record1List, record2List;
 			record1List = record1.getTaskRecord();
 			record2List = record2.getTaskRecord();
 
-			Assert::AreEqual(record1.getCommandType().c_str(), "add");
-			Assert::AreEqual(record2.getCommandType().c_str(), "delete");
-			Assert::AreEqual(record1List, taskList1);
-			Assert::AreEqual(record2List, taskList2);
+			Assert::AreEqual(record1.getCommandType(), commandType1);
+			Assert::AreEqual(record2.getCommandType(), commandType2);
+			
+			list<Task>::iterator it;
+			it = record1List.begin();
+			string name = it->getName();
+			Assert::AreEqual(name, name1);
+
+			it = record2List.begin();
+			name = it->getName();
+			Assert::AreEqual(name, name1);
+			it++;
+			name = it->getName();
+			Assert::AreEqual(name, name2);
 		}
 
 		TEST_METHOD(RecordSetCommandTypeTest){
-			Task task1 = Task("add", "CS meeting");
-			Task task2 = Task("add", "CS lecture");
+			string name1, name2, command1, command2;
+			name1 = "CS meeting";
+			name2 = "Cs lecture";
+			command1 = "add";
+			command2 = "add";
+			Task task1 = Task(command1, name1);
+			Task task2 = Task(command2, name2);
+
 			list<Task> taskList1, taskList2;
 			taskList1.push_back(task1);
 
 			taskList2.push_back(task1);
 			taskList2.push_back(task2);
 
-			Record record1 = Record("add", task1);
-			Record record2 = Record("delete", taskList2);
+			string commandType1, commandType2;
+			commandType1 = "add";
+			commandType2 = "delete";
 
-			record1.setCommandTypeRecord("done");
-			record2.setCommandTypeRecord("edit");
+			Record record1 = Record(commandType1, task1);
+			Record record2 = Record(commandType2, taskList2);
 
-			Assert::AreEqual(record1.getCommandType().c_str(), "done");
-			Assert::AreEqual(record2.getCommandType().c_str(), "edit");
+			string command3, command4;
+			command3 = "done";
+			command4 = "edit";
+			record1.setCommandTypeRecord(command3);
+			record2.setCommandTypeRecord(command4);
+
+			Assert::AreEqual(record1.getCommandType(), command3);
+			Assert::AreEqual(record2.getCommandType(), command4);
 		}
 
 		TEST_METHOD(RecordsetlistTaskTest){
-			Task task1 = Task("add", "CS meeting");
-			Task task2 = Task("add", "CS lecture");
-			Task task3 = Task("add", "Presentation");
+			string name1, name2, name3, command1, command2, command3;
+			name1 = "CS meeting";
+			name2 = "Cs lecture";
+			name3 = "Presentation";
+			command1 = "add";
+			command2 = "add";
+			command3 = "add";
+			Task task1 = Task(command1,name1);
+			Task task2 = Task(command2,name2);
+			Task task3 = Task(command3,name3);
 
 			list<Task> taskList1, taskList2;
 			taskList1.push_back(task1);
 
 			taskList2.push_back(task1);
 			taskList2.push_back(task2);
+			
+			string commandType1, commandType2;
+			commandType1 = "add";
+			commandType2 = "delete";
 
-			Record record1 = Record("add", task1);
-			Record record2 = Record("delete", taskList2);
+			Record record1 = Record(commandType1, task1);
+			Record record2 = Record(commandType2, taskList2);
 
 			record1.setlistTaskRecord(task3);
 			record2.setlistTaskRecord(task3);
@@ -77,7 +121,7 @@ namespace UnitTest_Jingyi
 			Assert::AreEqual(record2Size, 3);
 		}
 	};
-
+	
 	//@author A0115131B
 	TEST_CLASS(TrackerTest)
 	{
@@ -85,17 +129,26 @@ namespace UnitTest_Jingyi
 		
 		TEST_METHOD(TrackerIsEmptyTest)
 		{
-			// TODO: Your test code here
-			Task task1 = Task("add", "CS meeting");
-			Task task2 = Task("add", "CS lecture");
+			string name1, name2, command1, command2;
+			name1 = "CS meeting";
+			name2 = "Cs lecture";
+			command1 = "add";
+			command2 = "add";
+			Task task1 = Task(command1, name1);
+			Task task2 = Task(command2, name2);
+
 			list<Task> taskList1, taskList2;
 			taskList1.push_back(task1);
 
 			taskList2.push_back(task1);
 			taskList2.push_back(task2);
 
-			Record record1 = Record("add", task1);
-			Record record2 = Record("delete", taskList2);
+			string commandType1, commandType2;
+			commandType1 = "add";
+			commandType2 = "delete";
+
+			Record record1 = Record(commandType1, task1);
+			Record record2 = Record(commandType2, taskList2);
 
 			Tracker tracker1;
 			Assert::IsTrue(tracker1.isEmptyTracker());
@@ -107,38 +160,68 @@ namespace UnitTest_Jingyi
 		}
 
 		TEST_METHOD(TrackerGetNewestRecordTest){
-			Task task1 = Task("add", "CS meeting");
-			Task task2 = Task("add", "CS lecture");
+			string name1, name2, command1, command2;
+			name1 = "CS meeting";
+			name2 = "Cs lecture";
+			command1 = "add";
+			command2 = "add";
+			Task task1 = Task(command1, name1);
+			Task task2 = Task(command2, name2);
+
 			list<Task> taskList1, taskList2;
 			taskList1.push_back(task1);
 
 			taskList2.push_back(task1);
 			taskList2.push_back(task2);
 
-			Record record1 = Record("add", task1);
-			Record record2 = Record("delete", taskList2);
+			string commandType1, commandType2;
+			commandType1 = "add";
+			commandType2 = "delete";
+
+			Record record1 = Record(commandType1, task1);
+			Record record2 = Record(commandType2, taskList2);
 
 			Tracker tracker1;
 			tracker1.addRecord(record1);
 			Record record3 = tracker1.getNewestRecord();
-			Assert::AreEqual(record3, record1);
+			string name3, name4, name5, commandType3, commandType4;
+			name3 = record3.getTaskRecord().begin()->getName();
+			commandType3 = record3.getCommandType();
+			Assert::AreEqual(name3, name1);
+			Assert::AreEqual(commandType3, commandType1);
+
 
 			tracker1.addRecord(record2);
 			Record record4 = tracker1.getNewestRecord();
-			Assert::AreEqual(record4, record2);
+			name4 = record4.getTaskRecord().begin()->getName();
+			Assert::AreEqual(name4, name1);
+			name5 = record4.getTaskRecord().back().getName();
+			Assert::AreEqual(name5, name2);
+			commandType4 = record4.getCommandType();
+			Assert::AreEqual(commandType4, commandType2);
 		}
 
 		TEST_METHOD(deleteLastTrackerEntryTest){
-			Task task1 = Task("add", "CS meeting");
-			Task task2 = Task("add", "CS lecture");
+			string name1, name2, command1, command2;
+			name1 = "CS meeting";
+			name2 = "Cs lecture";
+			command1 = "add";
+			command2 = "add";
+			Task task1 = Task(command1, name1);
+			Task task2 = Task(command2, name2);
+
 			list<Task> taskList1, taskList2;
 			taskList1.push_back(task1);
 
 			taskList2.push_back(task1);
 			taskList2.push_back(task2);
+			
+			string commandType1, commandType2;
+			commandType1 = "add";
+			commandType2 = "delete";
 
-			Record record1 = Record("add", task1);
-			Record record2 = Record("delete", taskList2);
+			Record record1 = Record(commandType1, task1);
+			Record record2 = Record(commandType2, taskList2);
 
 			Tracker tracker1;
 			tracker1.addRecord(record1);
@@ -146,7 +229,11 @@ namespace UnitTest_Jingyi
 			
 			tracker1.deleteLastTrackerEntry();
 			Record record3 = tracker1.getNewestRecord();
-			Assert::AreEqual(record3, record1);
+			string name3, name4, commandType3;
+			name3 = record3.getTaskRecord().begin()->getName();
+			commandType3 = record3.getCommandType();
+			Assert::AreEqual(name3, name1);
+			Assert::AreEqual(commandType3, commandType1);
 		}
 
 	};
